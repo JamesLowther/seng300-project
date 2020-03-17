4a47bf8351be7cf79222c0cb3044275202766480
James Lowther <jamesevlowther@gmail.com>
2020-03-16 19:28:26 -0600

    Add Reviewer scenes

--- src/sengproject/gui/reviewer/ReviewerMenuScene.java
+++ src/sengproject/gui/reviewer/ReviewerMenuScene.java
@@ -5,49 +5,134 @@ import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.*;
 import javafx.scene.text.Font;
 import sengproject.Globals;
 import sengproject.gui.GuiController;
 import sengproject.gui.LoginScene;
+import sengproject.gui.reviewer.tvobjects.ReviewerPaper;
+import sengproject.login.LoginHandler;
+
+import java.util.ArrayList;
 
 public class ReviewerMenuScene {
 
-    public static Scene getScene() {
+    public static Scene getScene () {
 
-        // pane text
-        Label scene_title = new Label("Welcome back " + (String) Globals.getUser().get("username") + "!");
-        scene_title.setFont(new Font("Arial", 30));
-        VBox title = new VBox(scene_title);
-        title.setAlignment(Pos.TOP_CENTER);
-        title.setPadding(new Insets(0, 50, 10, 50));
-        title.setSpacing(10);
+        // name box
+        Label name_lb = new Label((String) Globals.getUser().get("username"));
+        name_lb.setFont(new Font("Arial", 24));
+        Label role_lb = new Label((String) Globals.getUser().get("role"));
+        role_lb.setFont(new Font("Arial", 18));
 
-        // pane text
-        Label todo_text = new Label("//todo: reviewer menu");
-        todo_text.setFont(new Font("Arial", 15));
+        VBox name_vb = new VBox();
+        name_vb.getChildren().addAll(name_lb, role_lb);
+        name_vb.setAlignment(Pos.TOP_LEFT);
 
-        // return button
-        Button return_button = new Button("Logout");
-        return_button.setAlignment(Pos.BOTTOM_CENTER);
-        return_button.setDefaultButton(true);
-        return_button.setOnAction(action -> {
-
+        // logout button
+        Button logout_b = new Button("Logout");
+        logout_b.setPrefSize(70, 40);
+        logout_b.setOnAction(action -> {
+            LoginHandler.logout();
             GuiController.changeScene(LoginScene.getScene());
-
         });
 
-        //vertical container
-        VBox elements = new VBox();
-        elements.getChildren().addAll(title, todo_text, return_button);
-        elements.setSpacing(70);
-        elements.setAlignment(Pos.CENTER);
+        // top spacer
+        Region top_spacer_r = new Region();
+        HBox.setHgrow(top_spacer_r, Priority.ALWAYS);
 
-        StackPane pane = new StackPane();
-        pane.getChildren().add(elements);
+        // top hbox
+        HBox top_hb = new HBox();
+        top_hb.setPadding(new Insets(10, 10, 10, 10));
+        top_hb.getChildren().addAll(name_vb, top_spacer_r, logout_b);
 
-        return new Scene(pane);
+
+        // browser papers button
+        Button browser_reviewers_b = new Button("Browse Papers");
+        browser_reviewers_b.setPrefSize(150, 40);
+        browser_reviewers_b.setOnAction(action -> {
+            // todo: call browse paper scene
+        });
+
+        // bottom spacer
+        Region bottom_spacer_r = new Region();
+        HBox.setHgrow(bottom_spacer_r, Priority.ALWAYS);
+
+        // bottom hbox
+        HBox bottom_hb = new HBox();
+        bottom_hb.setPadding(new Insets(10, 50, 10, 50));
+        bottom_hb.getChildren().addAll(bottom_spacer_r, browser_reviewers_b);
+
+        //  'your papers' label
+        Label center_lb = new Label("Papers you are currently reviewing");
+        center_lb.setFont(new Font("Arial", 16));
+
+        // table view
+        TableView<ReviewerPaper> papers_tv = new TableView<ReviewerPaper>();
+
+        TableColumn<ReviewerPaper, String> title_column = new TableColumn<ReviewerPaper, String>("Title");
+        title_column.setCellValueFactory(new PropertyValueFactory<ReviewerPaper, String>("title"));
+
+        TableColumn<ReviewerPaper, Button> details_column = new TableColumn<ReviewerPaper, Button>("");
+        details_column.setStyle("-fx-alignment: CENTER;");
+        details_column.setCellValueFactory(new PropertyValueFactory<ReviewerPaper, Button>("details_b"));
+        details_column.setSortable(false);
+
+        TableColumn<ReviewerPaper, Button> actions_column = new TableColumn<ReviewerPaper, Button>("");
+        actions_column.setStyle("-fx-alignment: CENTER;");
+        actions_column.setCellValueFactory(new PropertyValueFactory<ReviewerPaper, Button>("actions_b"));
+        actions_column.setSortable(false);
+
+        papers_tv.getColumns().addAll(title_column, details_column, actions_column);
+
+        papers_tv.getItems().addAll(getReviewerPapers()); // add papers to tableview
+
+        // center vbox
+        VBox center_vb = new VBox();
+        center_vb.setPadding(new Insets(10, 10, 10, 10));
+        center_vb.getChildren().addAll(center_lb, papers_tv);
+
+
+        BorderPane main_pane = new BorderPane();
+
+        main_pane.setTop(top_hb);
+        main_pane.setCenter(center_vb);
+        main_pane.setBottom(bottom_hb);
+
+
+        return new Scene(main_pane);
+
+    }
+
+    private static ArrayList<ReviewerPaper> getReviewerPapers () {
+
+        //todo: this will all be replaced
+        ArrayList<ReviewerPaper> papers = new ArrayList<ReviewerPaper>();
+
+        papers.add(new ReviewerPaper("Test paper 1", "21232", "01/01/2020", "John Doe",
+                "12319", "Journal of Smart", "123", "1231",
+                "test_2121.pdf", "02/02/2020", "03/03/2020", "3",
+                "23", "accepted"));
+        papers.add(new ReviewerPaper("Test paper 2", "21232", "01/01/2020", "John Doe",
+                "12319", "Journal of Smart", "123", "1231",
+                "test_2121.pdf", "02/02/2020", "03/03/2020", "3",
+                "23", "accepted"));
+
+        papers.add(new ReviewerPaper("Test paper 3", "21232", "01/01/2020", "John Doe",
+                "12319", "Journal of Smart", "123", "1231",
+                "test_2121.pdf", "02/02/2020", "03/03/2020", "3",
+                "23", "accepted"));
+
+        papers.add(new ReviewerPaper("Test paper 4", "21232", "01/01/2020", "John Doe",
+                "12319", "Journal of Smart", "123", "1231",
+                "test_2121.pdf", "02/02/2020", "03/03/2020", "3",
+                "23", "accepted"));
+
+
+        return papers;
 
     }
 }
---
+++ src/sengproject/gui/reviewer/tvobjects/ReviewerAction.java
@@ -0,0 +1,6 @@
+package sengproject.gui.reviewer.tvobjects;
+
+public class ReviewerAction {
+
+    //todo: im not sure if this class is needed or if we can reuse the ResearcherAction class
+}
