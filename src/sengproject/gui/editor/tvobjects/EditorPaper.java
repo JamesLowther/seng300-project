e0ee6c63331b166648f9b1451f1226f5d1a1375a
James Lowther <jamesevlowther@gmail.com>
2020-03-30 18:47:22 -0600

    Create menu scene and browse reviewer scene for editor

---
+++ src/sengproject/gui/editor/tvobjects/EditorPaper.java
@@ -0,0 +1,127 @@
+package sengproject.gui.editor.tvobjects;
+
+import javafx.scene.control.Button;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import sengproject.gui.GuiController;
+import sengproject.gui.researcher.ResearcherActionsScene;
+import sengproject.gui.researcher.ResearcherDetailsScene;
+import sengproject.gui.researcher.ResearcherReviewersScene;
+
+public class EditorPaper {
+
+    private JSONObject json_obj;
+
+    private Button details_b;
+    private Button reviewers_b;
+    private Button actions_b;
+
+    private String title;
+    private String paper_id;
+    private String sub_date;
+    private String author_name;
+    private String author_id;
+    private String journal_name;
+    private String journal_id;
+    private String volume_id;
+    private String file_name;
+    private String latest_date;
+    private String deadline;
+    private String reviewers;
+    private String status;
+
+    public EditorPaper(JSONObject paper) {
+
+        json_obj = paper;
+
+        title = (String) paper.get("title");
+        paper_id = (String) paper.get("paper_id").toString();
+        sub_date = (String) paper.get("sub_date");
+        author_name = (String) paper.get("author_name");
+        author_id = (String) paper.get("author_id");
+        journal_name = (String) paper.get("journal_name");
+        journal_id = (String) paper.get("journal_id");
+        volume_id = (String) paper.get("volume_id");
+        file_name = (String) paper.get("file_name");
+        latest_date = (String) paper.get("latest_date");
+        deadline = (String) paper.get("deadline");
+        reviewers = Integer.toString(((JSONArray) paper.get("reviewers")).size());
+        status = (String) paper.get("status");
+
+        details_b = new Button("Details");
+        details_b.setOnAction(action ->{
+            //todo: call details scene
+        });
+
+        reviewers_b = new Button("Reviewers");
+        reviewers_b.setOnAction(action ->{
+            //todo: call reviewers scene
+        });
+
+        actions_b = new Button("Actions");
+        actions_b.setOnAction(action ->{
+            //todo: call actions scene
+        });
+
+        System.out.println("paper created with title: " + title);
+    }
+
+    public JSONObject getJson_obj () { return json_obj; }
+
+    public void setTitle (String t) {
+        title = t;
+    }
+
+    public String getTitle () {
+        return title;
+    }
+
+    public void setDetails_b (Button b) {
+        details_b = b;
+    }
+
+    public Button getDetails_b () {
+        return details_b;
+    }
+
+    public void setReviewers_b (Button b) {
+        reviewers_b = b;
+    }
+
+    public Button getReviewers_b () {
+        return reviewers_b;
+    }
+
+    public void setActions_b (Button b) {
+        actions_b = b;
+    }
+
+    public Button getActions_b () {
+        return actions_b;
+    }
+
+    public String getPaper_id () {return paper_id;}
+
+    public String getSub_date () {return sub_date;}
+
+    public String getAuthor_name () {return author_name;}
+
+    public String getAuthor_id () {return author_id;}
+
+    public String getJournal_name () {return journal_name;}
+
+    public String getJournal_id () {return journal_id;}
+
+    public String getVolume_id () {return volume_id;}
+
+    public String getFile_name () {return file_name;}
+
+    public String getLatest_date () {return latest_date;}
+
+    public String getDeadline () {return deadline;}
+
+    public String getReviewers () {return reviewers;}
+
+    public String getStatus () {return status;}
+
+}
---
+++ src/sengproject/gui/editor/tvobjects/EditorReviewer.java
@@ -0,0 +1,41 @@
+package sengproject.gui.editor.tvobjects;
+
+public class EditorReviewer {
+
+    private String name;
+    private long num_major_rev;
+    private long num_minor_rev;
+    private long num_pub_rev;
+    private double avg_rev_time;
+
+    public EditorReviewer (String n, long maj_r, long min_r, long pub_r, double avg_t) {
+
+        name = n;
+        num_major_rev = maj_r;
+        num_minor_rev = min_r;
+        num_pub_rev = pub_r;
+        avg_rev_time = avg_t;
+
+    }
+
+    public void setName (String n) {name = n;}
+
+    public String getName () {return name;}
+
+    public void setNum_major_rev (long n) {num_major_rev = n;}
+
+    public long getNum_major_rev () {return num_major_rev;}
+
+    public void setNum_minor_rev (long n) {num_minor_rev = n;}
+
+    public long getNum_minor_rev () {return num_minor_rev;}
+
+    public void setNum_pub_rev (long n) {num_pub_rev = n;}
+
+    public long getNum_pub_rev () {return num_pub_rev;}
+
+    public void setAvg_rev_time (double n) {avg_rev_time = n;}
+
+    public double getAvg_rev_time () {return avg_rev_time;}
+
+}
